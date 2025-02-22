1f4fedc9392dde3107705f44c8baa600af8acae8
Vidhi Jain <vidhijain507@gmail.com>
2025-02-22 10:24:56 +0530

    [Vidhi_Jain]Add. Helloworld Spring App for printing Hello.

---
+++ src/test/java/com/FirstSpringApp/FirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FirstSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
